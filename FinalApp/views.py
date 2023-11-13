9ea5b8f6e981eeead52485b3c80bbcab515080bb
Marvin Solano <marvinsolano83@gmail.com>
2023-11-13 17:17:43 -0500

    Capstone project setup

---
+++ FinalApp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .forms import CommentForm
+from .models import UserComments
+
+
+def form_view(request):
+    from = CommentForm()
+    if form.is_valid():
+        cd = form.cleaned_data
+        uc = UserComments(
+            first_name = cd['first_name'],
+            lsat_namme = cd['last_name'],
+            comment = cd['comment']
+            )
+        uc.save()
+        return JsonResponse({
+            'message': 'success'
+            })
+        
+    return render(request, 'blog.html', {'form': form})
