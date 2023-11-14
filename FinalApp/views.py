d2d3c5167d73b614b987078bab9be49a05569ccc
Marvin Solano <marvinsolano83@gmail.com>
2023-11-13 21:01:12 -0500

    Adding static and templates folder

--- FinalApp/views.py
+++ FinalApp/views.py
@@ -4,18 +4,22 @@ from .forms import CommentForm
 from .models import UserComments
 
 
-def form_view(request):
-    from = CommentForm()
-    if form.is_valid():
-        cd = form.cleaned_data
-        uc = UserComments(
-            first_name = cd['first_name'],
-            lsat_namme = cd['last_name'],
-            comment = cd['comment']
-            )
-        uc.save()
-        return JsonResponse({
-            'message': 'success'
-            })
+# def form_view(request):
+#     from = CommentForm()
+#     if form.is_valid():
+#         cd = form.cleaned_data
+#         uc = UserComments(
+#             first_name = cd['first_name'],
+#             lsat_namme = cd['last_name'],
+#             comment = cd['comment']
+#             )
+#         uc.save()
+#         return JsonResponse({
+#             'message': 'success'
+#             })
         
-    return render(request, 'blog.html', {'form': form})
+#     return render(request, 'blog.html', {'form': form})
+
+
+def index(request):
+    return render(request, 'index.html', {})
